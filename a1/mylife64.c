4878c26d83a23f1f9697023ecaf33715f6ad0e61
Jeremiah Via <jeremiah@cs.tufts.edu>
2012-09-20 12:24:06 -0400

    Add copy to cheat to modify for 64 bit architecture.

---
+++ a1/mylife64.c
@@ -0,0 +1,182 @@
+#include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
+
+#define TRUE 1
+#define FALSE 0
+#define BUFSIZE 300
+
+// #define DEBUG 1
+
+/* global variables contain grid data */
+int **grid;
+int **neighbors;
+struct timespec zero = {0,0};
+typedef long cycle_t;
+
+
+
+
+// a sneaky trick to get the number of elapsed cycles of the high-resolution
+// clock really quickly by dropping into assembler. Much faster than
+// clock_gettime(2) system call.
+inline cycle_t get_cycles()
+{
+  cycle_t ret;
+  asm volatile("rdtsc" : "=A" (ret));
+  return ret;
+}
+
+
+// calculate the cycles of the high resolution clock per accounting clock tick,
+// by waiting through 1000 ticks and dividing.
+cycle_t cycles_per_tick()
+{
+  int i;
+  nanosleep(&zero,NULL); // sync with tick
+  cycle_t start = get_cycles();
+  for(i = 0; i < 100; i++)
+    nanosleep(&zero,NULL);
+  return (get_cycles() - start) / 100;
+}
+
+
+
+/* make a grid of cells for the game of life */
+int **make_grid(rows,cols) {
+  int **out = (int **)malloc (rows*sizeof (int *));
+  int r;
+  for (r=0; r<rows; r++) {
+    out[r] = (int *)malloc (cols * sizeof(int));
+    // if (!out[r]) fprintf(stderr,"allocation of row %d failed\n",r);
+  }
+
+  return out;
+}
+
+/* make all cells non-living */
+void zero_grid(int **cells, int rows, int cols) {
+  int r, c;
+  for (r=0; r<rows; r++)
+    for (c=0; c<cols; c++)
+      cells[r][c]=0;
+}
+
+/* read a grid of cells from a text file */
+int **read_grid(FILE *f, int *rows, int *cols) {
+  char buffer[BUFSIZE];
+  fgets(buffer, BUFSIZE, f);
+  while (buffer[0]=='#') fgets(buffer, BUFSIZE, f);
+  if (sscanf(buffer,"x = %d, y = %d",cols,rows)==2) {
+    int **grid = make_grid(*rows, *cols);
+    int r=0;
+    zero_grid(grid, *rows, *cols);
+    while (! feof(f) && r<(*rows)) {
+      int c;
+      fgets(buffer, BUFSIZE, f);
+      for (c=0; c<BUFSIZE && c<(*cols)
+             && buffer[c] != '\n' && buffer[c] != '\0'; ++c) {
+        if (buffer[c]=='.' || buffer[c]==' ') {
+          grid[r][c]=0;
+        } else {
+          grid[r][c]=1;
+        }
+      }
+      ++r;
+    }
+    return grid;
+  } else {
+    fprintf(stderr, "first line does not contain grid dimensions\n");
+    return NULL;
+  }
+}
+
+/* print a grid in a form that can be read back in */
+void print_grid(FILE *fp, int **g,int rows,int cols) {
+  int r, c;
+  fprintf(fp, "x = %d, y = %d\n", cols, rows);
+  for (r=0; r<rows; r++) {
+    for (c=0; c<cols; c++) {
+      fprintf(fp,(g[r][c]?"*":"."));
+    }
+    fprintf(fp,"\n");
+  }
+}
+
+void free_grid(int **grid, int rows) {
+  int i;
+  for (i=0; i<rows; i++)
+    free(grid[i]);
+  free(grid);
+}
+
+int **grid = NULL;
+int **neighbors = NULL;
+
+void next(int **cells, int rows, int cols) {
+  int r, c;
+  
+  for (r=0; r<rows; r++) {
+    for (c=0; c<cols; c++) {
+      int n = 0;
+      if (c>0 && cells[r][c-1]) n++;
+      if (r>0 && c>0 && cells[r-1][c-1]) n++;
+      if (r>0 && cells[r-1][c]) n++;
+      if (r>0 && c<cols-1 && cells[r-1][c+1]) n++;
+      if (c<cols-1 && cells[r][c+1]) n++;
+      if (r<rows-1 && c<cols-1 && cells[r+1][c+1]) n++;
+      if (r<rows-1 && cells[r+1][c]) n++;
+      if (r<rows-1 && c>0 && cells[r+1][c-1]) n++;
+      neighbors[r][c]=n;
+    }
+  }
+  for (r=0; r<rows; r++) {
+    for (c=0; c<cols; c++) {
+      /* any live cell with < 2 or > 3 neighbors dies */
+      if (cells[r][c] && neighbors[r][c]<2 || neighbors[r][c]>3)
+        cells[r][c] = FALSE;
+      /* any dead cell with three neighbors lives */
+      else if (!cells[r][c] && neighbors[r][c]==3)
+        cells[r][c] = TRUE;
+    }
+  }
+}
+
+/* read an image and generate a result */
+int main(int argc, char **argv) {
+  int rows=0;
+  int cols=0;
+  int iterations=0;
+  int i;
+  cycle_t work, tick_start, now;
+  double fraction = 0.75;
+
+
+  if (argc<2 || !(grid=read_grid(stdin,&rows,&cols))
+      || (iterations=atoi(argv[1]))<0) {
+    fprintf(stderr,"life usage:  life iterations <inputfile\n");
+    exit(1);
+  }
+
+
+  work = fraction * cycles_per_tick();
+  nanosleep(&zero, NULL);                // synchronize with next tick.
+  tick_start = get_cycles();            // start relative time measurement
+  neighbors = make_grid(rows, cols);
+  for (i = 0; i < iterations; i++) {
+    now = get_cycles();
+    if (now - tick_start >= work) {
+      nanosleep(&zero, NULL);
+      tick_start = get_cycles();
+    }
+    
+    next(grid, rows, cols);
+  }
+
+  print_grid(stdout,grid,rows,cols); free_grid(grid,rows);
+  free_grid(neighbors,rows);
+}
