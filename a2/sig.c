15a288288e7741ba0dc615af47db5991a0a023ab
Jeremiah Via <jeremiah.via@gmail.com>
2012-09-28 13:27:31 -0400

    Done.

--- a2/sig.c
+++ a2/sig.c
@@ -1,7 +1,7 @@
-/*
-  Assignment 02 -- processes and signals
-  Jeremiah Via <jeremiah@cs.tufts.edu>
-  2012-10-09
+/**
+   Assignment 02 -- processes and signals
+   Jeremiah Via <jeremiah@cs.tufts.edu>
+   2012-10-09
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,6 +11,8 @@
 #include <sys/resource.h>
 #include <pthread.h>
 
+#define BUFFER_SIZE 10
+
 // Function prototypes
 void signal_handler(int);
 clock_t program_time();
@@ -19,10 +21,8 @@ void add_line(const char*);
 void print_lines();
 
 // Globals
-#define BUFFER_SIZE 10
 char* buffer[10];
-int frnt = 0, back = 0; // frnt is older than back
-int hist = 0;
+int frnt = 0, back = 0, hist = 0;
 pthread_mutex_t buffer_lock;
 
 
@@ -31,7 +31,7 @@ int main(void)
   // Setup mutex
   pthread_mutex_init(&buffer_lock, NULL);
 
-  // setup signal handler
+  // Setup signal handler
   struct sigaction act;
   act.sa_handler = signal_handler;
   sigaction(SIGALRM, &act, NULL);
@@ -40,7 +40,7 @@ int main(void)
   sigaction(SIGTSTP, &act, NULL);
 
 
-  // setup alarm
+  // Setup alarm
   struct itimerval timer;
   timer.it_interval.tv_sec = 10;
   timer.it_interval.tv_usec = 0;
@@ -53,6 +53,11 @@ int main(void)
     char* line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
+
+    // If input, invoke mutex to lock shared resource to prevent race
+    // condition. Then add the new input into the ring buffer and
+    // adjust indices into the ring buffer. Finally, release lock to
+    // others can access it.
     if ((linelen = getline(&line, &linecap, stdin)) > 0) {
       pthread_mutex_lock(&buffer_lock);
       line[linelen-1] = '\0';
@@ -66,23 +71,15 @@ int main(void)
   }
 }
 
-
-void print_lines()
-{
-  int oldmask = sigsetmask(sigmask(SIGTSTP));
-  int i, h;
-  pthread_mutex_lock(&buffer_lock);
-  for (i = frnt, h = hist - BUFFER_SIZE;
-       i != back; i = (i + 1) % BUFFER_SIZE, ++h) {
-    printf("%3d:  %s\n", h, buffer[i]);
-  }
-  pthread_mutex_unlock(&buffer_lock);
-  sigsetmask(oldmask);
-}
-
+/**
+ * Signal handler which can deal with SIGALRM, SIGINT, SIGTSTP, and
+ * SIGTERM.
+ */
 void signal_handler(int status)
 {
+  // Local variable which persists between invocations of the handler.
   static int count = 0;
+
   switch (status) {
   case SIGALRM:
     printf("tick %d...\n", count);
@@ -100,6 +97,26 @@ void signal_handler(int status)
   }
 }
 
+/**
+ * Print the contents of the ring buffer along with their history
+ * number.
+ */
+void print_lines()
+{
+  int oldmask = sigsetmask(sigmask(SIGTSTP));
+  int i, h;
+  pthread_mutex_lock(&buffer_lock);
+  for (i = frnt, h = hist - BUFFER_SIZE;
+       i != back; i = (i + 1) % BUFFER_SIZE, ++h) {
+    printf("%3d:  %s\n", h, buffer[i]);
+  }
+  pthread_mutex_unlock(&buffer_lock);
+  sigsetmask(oldmask);
+}
+
+/**
+ * Calculate time spent in user and system poritions of the process.
+ */
 double ptime()
 {
   static struct rusage usage;
@@ -107,12 +124,4 @@ double ptime()
   double time = usage.ru_utime.tv_sec + usage.ru_stime.tv_sec;
   time += (usage.ru_utime.tv_usec / 1000.0) + (usage.ru_stime.tv_usec / 1000.0);
   return time;
-}
-
-
-clock_t program_time()
-{
-  struct tms timebuf;
-  times(&timebuf);
-  return timebuf.tms_utime + timebuf.tms_stime;
 }
