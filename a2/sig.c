4f51305c8686bfe5ee18fdc4d389413ecd378dd5
Jeremiah Via <jeremiah@cs.tufts.edu>
2012-10-09 15:33:35 -0400

    Seems to work now. Time for clean up.

--- a2/sig.c
+++ a2/sig.c
@@ -11,7 +11,7 @@
 #include <sys/resource.h>
 #include <pthread.h>
 
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 11
 
 // Function prototypes
 void signal_handler(int);
@@ -21,46 +21,53 @@ void add_line(const char*);
 void print_lines();
 
 // Globals
-char* buffer[10];
+char* buffer[BUFFER_SIZE];
 int frnt = 0, back = 0, hist = 0;
 pthread_mutex_t buffer_lock;
+struct sigaction act;
 
 
 int main(void)
 {
   // Setup mutex
-  pthread_mutex_init(&buffer_lock, NULL);
+  if (pthread_mutex_init(&buffer_lock, NULL) != 0) {
+    fprintf(stderr, "Could not create mutex...exiting\n");
+    exit(1);
+  }
 
   // Setup signal handler
-  struct sigaction act;
   sigemptyset(&act.sa_mask);
   sigaddset(&act.sa_mask, SIGTSTP);
+  sigaddset(&act.sa_mask, SIGALRM);
   act.sa_handler = signal_handler;
+  act.sa_flags = SA_RESTART;
   sigaction(SIGALRM, &act, NULL);
   sigaction(SIGINT,  &act, NULL);
   sigaction(SIGTERM, &act, NULL);
   sigaction(SIGTSTP, &act, NULL);
 
-
   // Setup alarm
   struct itimerval timer;
   timer.it_interval.tv_sec = 10;
-  timer.it_interval.tv_usec = 0;
-  timer.it_value.tv_sec = 10;
-  timer.it_value.tv_usec = 0;
+  timer.it_interval.tv_usec = 1;
+  timer.it_value = timer.it_interval;
   setitimer(ITIMER_REAL, &timer, NULL);
 
   // Capture user input and place into ring buffer
   while(1) {
     char* line = NULL;
-    size_t linecap = 0;
+    size_t len = 0;
     ssize_t linelen;
 
     // If input, invoke mutex to lock shared resource to prevent race
     // condition. Then add the new input into the ring buffer and
     // adjust indices into the ring buffer. Finally, release lock to
     // others can access it.
-    if ((linelen = getline(&line, &linecap, stdin)) > 0) {
+    linelen = getline(&line, &len, stdin);
+    fflush(stdin);
+    if (linelen >= 0) printf("LEN: %d\n",linelen);
+    if (linelen != -1) {
+      fprintf(stderr,"got it\n");
       pthread_mutex_lock(&buffer_lock);
       line[linelen-1] = '\0';
       if (back < frnt)
@@ -96,7 +103,7 @@ void signal_handler(int status)
     print_lines();
     break;
   case SIGTERM:
-    printf("Program time: %f\n", ptime());
+    printf("Program time: %f\nCleaning up...\n", ptime());
     for (; frnt != back; frnt = (frnt + 1) % BUFFER_SIZE)
       free(buffer[frnt]);
     exit(0);
@@ -111,10 +118,10 @@ void print_lines()
 {
   int i, h;
   pthread_mutex_lock(&buffer_lock);
-  for (i = frnt, h = hist - BUFFER_SIZE;
-       i != back; i = (i + 1) % BUFFER_SIZE, ++h) {
-    printf("%3d:  %s\n", h, buffer[i]);
+  for (i = frnt; i != back; i = (i + 1) % BUFFER_SIZE, ++h) {
+    printf("  %s\n", buffer[i]);
   }
+  fflush(stdout);
   pthread_mutex_unlock(&buffer_lock);
 }
 
