39fd15363ac3f1387bb407a4b2988d5862f0e6dc
Jeremiah Via <jeremiah.via@gmail.com>
2012-12-08 18:54:18 -0500

    May remove stddev code.

--- a6/src/a6.c
+++ a6/src/a6.c
@@ -13,11 +13,11 @@
 #include <time.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+#include <sys/stat.h>
 #include <unistd.h>
 
-#define PAGE_SIZE  8096
-#define WRITE_SIZE PAGE_SIZE*100
-#define BUFFER_SIZE PAGE_SIZE
+#define PAGE_SIZE  4096
+#define BUFFER_SIZE 4096
 
 #define TIMES 100
 
@@ -28,14 +28,43 @@ typedef struct _res {
 
 //////////////////////////////////////////////////////////////////////
 // Proto Declarations
-double avg(double*,int);
+long avg(long*,int);
+long stddev(long*, int);
 
 FILE *file;
 char *buffer;
+long file_size;
 
-double to_secs(struct timeval start, struct timeval end) {
-  return ((double) end.tv_sec - start.tv_sec)
-    +    ((double) (end.tv_usec - start.tv_usec) / 1000.);
+
+long avg(long *times, int len)
+{
+  long avg = 0L;
+  int i;
+  for (i = 0; i < len; i++) {
+    avg += times[i];
+  }
+  return (avg / len);
+}
+
+long stddev(long *times, int len)
+{
+  long stddev = 0L;
+  long u = avg(times, len);
+
+  // calculate variance
+  double var = 0L;
+  int i;
+  printf ("mean = %ld\n", u);
+  for (i = 0; i < len; ++i) {
+    printf("%2d => %ld; %ld; %f\n", i, times[i], times[i] - u, pow(times[i] - u, 2.0));
+    var += pow(times[i] - u, 2.0);
+  }
+  var /= len;
+
+  // calculate stddev
+  stddev = sqrt(var);
+ 
+  return stddev;
 }
 
 /**
@@ -45,27 +74,31 @@ double to_secs(struct timeval start, struct timeval end) {
  */
 result benchmark_disk_read(const char *path)
 {
-  result res = {0, 0} ;
-  int fd;
   struct timespec start, end;
-  double times[TIMES];
+  long times[TIMES];
 
   // prepare
-  fd = open(path, O_RDONLY, 0);
-  buffer = malloc(BUFFER_SIZE*sizeof(char));
+  int fd = open(path, O_RDONLY, 0);
+  buffer = malloc(BUFFER_SIZE * sizeof(char));
 
-  // run
+  // run becnhmark
   int i;
-  clock_gettime(CLOCK_REALTIME, &start);
-  read(fd, &buffer, BUFFER_SIZE);
-  clock_gettime(CLOCK_REALTIME, &end);
+  for (i = 0; i < TIMES; ++i) {
+    sync();
+    clock_gettime(CLOCK_REALTIME, &start);
+    read(fd, &buffer, BUFFER_SIZE);
+    clock_gettime(CLOCK_REALTIME, &end);
+    times[i] = end.tv_nsec - start.tv_nsec;
+  }
     
   // clean up
   free(buffer);
   close(fd);
 
   // calculate
-  res.time = end.tv_nsec - start.tv_nsec;
+  result res = {0, 0};
+  res.time = avg(times, TIMES);
+  res.stddev = stddev(times, TIMES);
   return res;
 }
 
@@ -76,19 +109,36 @@ result benchmark_disk_read(const char *path)
  */
 result benchmark_disk_write(const char *path)
 {
-  result res = {0, 0};
+  struct timespec start, end;
+  long times[TIMES];
+  
+  // prepare
   int fd = open(path, O_WRONLY, 0);
+  buffer = malloc(BUFFER_SIZE * sizeof(char));
+
+  // populate buffer for writing
   int i;
   for (i = 0; i < BUFFER_SIZE; ++i) {
     buffer[i] = 'a';
   }
 
-  struct timespec start, end;
-  clock_gettime(CLOCK_REALTIME, &start);
-  write(fd, &buffer, BUFFER_SIZE);
-  clock_gettime(CLOCK_REALTIME, &end);
-  res.time = end.tv_nsec - start.tv_nsec;
+  // run becnhmark
+  for (i = 0; i < TIMES; ++i) {
+    sync();
+    clock_gettime(CLOCK_REALTIME, &start);
+    write(fd, &buffer, BUFFER_SIZE);
+    clock_gettime(CLOCK_REALTIME, &end);
+    times[i] = end.tv_nsec - start.tv_nsec;
+  }
+
+  // clean up
+  free(buffer);
   close(fd);
+
+  // calculate
+  result res = {0, 0};
+  res.time = avg(times, TIMES);
+  res.stddev = stddev(times, TIMES);
   return res;
 }
 
@@ -102,17 +152,27 @@ result benchmark_cache_read(const char *path)
   result res = {0, 0};
   int fd;
   struct timespec start, end;
-  double time[TIMES];
-  
+  long times[TIMES];
+
+  // open and read once to move into cache
   fd = open(path, O_RDONLY, 0);
   buffer = malloc(BUFFER_SIZE*sizeof(char));
   read(fd, &buffer, BUFFER_SIZE);
-  clock_gettime(CLOCK_REALTIME, &start);
-  read(fd, &buffer, BUFFER_SIZE);
-  clock_gettime(CLOCK_REALTIME, &end);
+
+  // benchmark
+  int i;
+  for (i = 0; i < TIMES; ++i) {
+    clock_gettime(CLOCK_REALTIME, &start);
+    read(fd, &buffer, BUFFER_SIZE);
+    clock_gettime(CLOCK_REALTIME, &end);
+    times[i] = end.tv_nsec - start.tv_nsec;
+  }
+
+  // cleaup
   free(buffer);
-  res.time = end.tv_nsec - start.tv_nsec;
   close(fd);
+
+  res.time = avg(times, TIMES);
   return res;
 }
 
@@ -140,34 +200,22 @@ result benchmark_cache_write(const char *path)
   return res;
 }
 
-/**
- * Randomly read/write to play around with the cache and prime it for
- * another benchmark.
- */
-void play_with_cache(void)
-{
-  /* int fd = open("testfile.dat", O_RDONLY, 0); */
-  /* buffer = malloc(BUFFER_SIZE * sizeof(char)); */
-
-  /* int i; */
-  /* for (i = 0; i < 10; ++i) { */
-    
-  /*   lseek(fd,  */
-  /* } */
-}
-
 int main(int argc, char **argv)
 {
-
   if (argc != 2) {
     fprintf(stderr, "usage: %s <file>\n", argv[0]);
     exit(1);
   }
-  
+
   result disk_read   = benchmark_disk_read(argv[1]);
   result disk_write  = benchmark_disk_write(argv[1]);
   result cache_read  = benchmark_cache_read(argv[1]);
   result cache_write = benchmark_cache_write(argv[1]);
+
+  // Stuff to print for fun, while debuggin
+  printf ("\n--------------------\n");
+  printf ("Page size: %d\n" , getpagesize());
+  printf ("--------------------\n");
   
   printf ("Disk Read  \n  Average: %ld ns\n  Stddev:  %ld ns\n", disk_read.time,   disk_read.stddev);
   printf ("Disk Write \n  Average: %ld ns\n  Stddev:  %ld ns\n", disk_write.time,  disk_write.stddev);
